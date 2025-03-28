0d5712273cc2c43163acc534b760701306e0024b
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-03-28 10:54:31 +0100

    Add more operatorTypes

--- src/main/java/dk/dtu/compute/course02324/mini_java/semantics/ProgramTypeVisitor.java
+++ src/main/java/dk/dtu/compute/course02324/mini_java/semantics/ProgramTypeVisitor.java
@@ -16,9 +16,14 @@ public class ProgramTypeVisitor extends ProgramVisitor {
      * types of all operands and the result of te operation are the same.
      */
     final private Map<Operator,List<Type>> operatorTypes = Map.ofEntries(
+            entry(PLUS1, List.of(INT,FLOAT)),
+            entry(MINUS1, List.of(INT,FLOAT)),
             entry(PLUS2, List.of(INT, FLOAT)),
             entry(MINUS2, List.of(INT, FLOAT)),
-            entry(MULT, List.of(INT, FLOAT)));
+            entry(MULT, List.of(INT, FLOAT)),
+            entry(DIV, List.of(INT,FLOAT)),
+            //Float? Same for MINUS2 and MULT
+            entry(MOD, List.of(INT)));
 
     final public Map<Expression, Type> typeMapping = new HashMap<>();
 
